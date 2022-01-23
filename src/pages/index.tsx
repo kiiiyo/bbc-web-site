5fc57d93b60aa9a3d6c1ef300132c8350f0d40c5
kiyota.takashi <kiyota.takashi@classmethod.jp>
2022-01-23 20:24:18 +0900

    :wrench: modify tsconfig path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-  return <div>Home Page</div>
-}
+import { Pages } from '@/components'
 
 //<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
 
-export default Home
+const HomePage: NextPage = () => {
+  return <Pages.HomePage />
+}
+
+export default HomePage
