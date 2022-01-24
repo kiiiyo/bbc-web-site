860d41f0bc00a5465de88fee153977f917058672
kiyota.takashi <kiyota.takashi@classmethod.jp>
2022-01-24 11:03:45 +0900

    :sparkles: add Local HOC Component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
+import Head from 'next/head'
+//
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function CustomApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default CustomApp
