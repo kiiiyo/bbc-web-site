aa8a2b0c73eede46d01454725aa413f94e5d62f3
kiyota.takashi <kiyota.takashi@classmethod.jp>
2022-01-25 13:54:47 +0900

    :lipstick: add find page

--- src/components/organisms/index.tsx
+++ src/components/organisms/index.tsx
@@ -2,6 +2,6 @@ import { Header } from './Header'
 import { Footer } from './Footer'
 import { Hero } from './Hero'
 import { RoadmapSection } from './RoadmapSection'
-
+import { FindSection } from './FindSection'
 //
-export { Header, Footer, Hero, RoadmapSection }
+export { Header, Footer, Hero, RoadmapSection, FindSection }
