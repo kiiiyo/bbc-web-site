8c0cc90f05f02fb91bf2e0075eb8c729ecfab9cf
kiyota.takashi <kiyota.takashi@classmethod.jp>
2022-02-12 22:34:10 +0900

    :speech_balloon: modify text

--- src/env/language/ja.tsx
+++ src/env/language/ja.tsx
@@ -48,10 +48,10 @@ export const language: TLanguage = {
           description:
             'オフィシャルサイトオープン記念、サイト内に隠れているベアリーを見つけると抽選でNFTがもらえるイベントを実施中です!',
           buttonLabel: 'このページにいるかも',
-          currentPeople: '残り43人',
+          currentPeople: '残り38人',
           currentOwner: (
             <>
-              🐻 ベアリーが<strong className="text-xl font-bold"> 21名 </strong>の❤️
+              🐻 ベアリーが<strong className="text-xl font-bold"> 25名 </strong>の❤️
               オーナーさんにお迎えいただきました🎉
             </>
           )
@@ -186,10 +186,10 @@ export const language: TLanguage = {
               </>
             ),
 
-            currentPeople: '残り43人',
+            currentPeople: '残り38人',
             currentOwner: (
               <>
-                🐻 ベアリーが<strong className="text-xl font-bold"> 21名 </strong>の❤️
+                🐻 ベアリーが<strong className="text-xl font-bold"> 25名 </strong>の❤️
                 オーナーさんにお迎えいただきました🎉
               </>
             ),
