39985941daa1c0bfd9ec3b1018d146425c8289a8
kiyota.takashi <kiyota.takashi@classmethod.jp>
2022-02-14 21:36:23 +0900

    :construction: modify layout

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -2,6 +2,7 @@ import { HomePage } from './HomePage'
 import { AboutPage } from './AboutPage'
 import { FindPage } from './FindPage'
 import { RoadmapPage } from './RoadmapPage'
-import { CampaignPage } from './CampaignPage/'
+import { CampaignPage } from './CampaignPage'
+import { BearsPage } from './BearsPage'
 //
-export { HomePage, AboutPage, RoadmapPage, FindPage, CampaignPage }
+export { HomePage, AboutPage, RoadmapPage, FindPage, CampaignPage, BearsPage }
---
+++ src/pages/[locale]/bears/neo.tsx
@@ -0,0 +1,48 @@
+import type { NextPage } from 'next'
+import ErrorPage from 'next/error'
+//
+import { Context } from '@/features'
+//import { Pages } from '@/components'
+import { withLocale } from '@/utils/translations/locales'
+
+type State = {
+  page: {
+    path: string
+    preview: boolean
+    locale: 'ja' | 'en'
+  }
+}
+
+type AboutPageProps = {
+  state: State
+}
+
+export const getServerSideProps = withLocale(async (locale, { resolvedUrl }) => {
+  const path = String(resolvedUrl.split('?')) ?? '/'
+
+  // TODO: エラーハンドリング
+  const page = {
+    path,
+    locale
+  }
+
+  return {
+    props: {
+      state: { page }
+    }
+  }
+})
+
+const AboutPage: NextPage<AboutPageProps> = ({ state: { page } }) => {
+  if (!page) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  return (
+    <Context.LocaleContext.LocaleContextProvider value={{ locale: page.locale, path: page.path }}>
+      neo
+    </Context.LocaleContext.LocaleContextProvider>
+  )
+}
+
+export default AboutPage
